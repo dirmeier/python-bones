9d2234d26cd2bfc98961c83d85a00802f6620f0d
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-04-21 23:43:56 +0200

    added npm test and travis

--- test/test_app.js
+++ test/test_app.js
@@ -1,26 +1,30 @@
 'use strict';
 
-var path    = require("path");
-var assert  = require("yeoman-assert");
+var path = require("path");
+var assert = require("yeoman-assert");
 var helpers = require("yeoman-test");
 
-beforeEach(function () {  
-  return helpers.run(path.join(__dirname, "../app"))
-})
+beforeEach(function () {
+    return helpers.run(path.join(__dirname, "../app"))
+});
 
-describe("java-bones:app", function () {
-  it("builds files", function () {
-    assert.file([
-      "LICENSE", 
-      "README.md", 
-      "TODO.md", 
-      "VERSIONS.md", 
-      ".gitignore", ".gitattributes", 
-      ".travis.yml", ".codecov.yml",     
-      "build.sh", "emptyproject4j", 
-      "checkstyle.xml", "ruleset.xml",       
-      "pom.xml",
-      "src/main/java/net/groupID/emptyproject4j/Main.java",
-      "src/test/java/net/groupID/tests/emptyproject4j/TestSuite.java"]);
-  });
+describe("python-bones:app", function () {
+    it("builds files", function () {
+        assert.file([
+            "LICENSE",
+            "README.rst",
+            "TODO.md",
+            "VERSIONS.md",
+            "MANIFEST.in",
+            ".gitignore", ".gitattributes",
+            ".travis.yml", ".codecov.yml", ".coveragerc", ".pylintrc",
+            "bld.dat", "build.sh", "install.sh",
+            "build.sh", "emptyproject", "meta.yaml", "requirements.txt",
+            "setup.py", "setup.cfg",
+            "test.sh", "tests/test_me.py",
+            "docs/MakeFile", "docs/source/conf.py", "docs/source/examples.rst",
+            "docs/source/index.rst", "docs/source/modules.rst", "docs/source/tutorial.rst",
+            "emptyproject/__init__.py", "emptyproject/emptyproject.py"
+        ]);
+    });
 });
